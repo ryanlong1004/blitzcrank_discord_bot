8513290f88e72c68fd70b69e42bdfb01d59671d7
Ryan Long <ryan@ryanlong.tech>
2020-12-28 14:51:08 -0500

    Finished initial implementation

--- src/blitzcrank/cogs/rss.py
+++ src/blitzcrank/cogs/rss.py
@@ -1,33 +1,40 @@
 import feedparser
 import re
-from discord import Embed
+from discord import Embed, Webhook, RequestsWebhookAdapter
 
 
-class Podcast:
+class Podcast(dict):
     PATTERN = r"(src=\")(\S*)(\")"
-
-    def __init__(self, id, link, image):
-        self.id = id
-        self.ink = link
-        self.image = image
 
     def __repr__(self):
         return f"{self.__dict__.items()}"
 
-    def __get__(self, instance, owner):
-        pass
-
     @classmethod
     def from_feed(cls, url):
         results = feedparser.parse(url)
-        id = results["entries"][0]["id"]
-        link = results["entries"][0]["link"]
-        image = re.search(cls.PATTERN, results["entries"][0]["summary"]).group(2)
-        return Podcast(id, link, image)
+        return Podcast(
+            {
+                "id": results["entries"][0]["id"],
+                "url": results["entries"][0]["link"],
+                "description": results["entries"][0]["subtitle"],
+                "title": f"{results['entries'][0]['itunes_title']} ({results['entries'][0]['itunes_episode']})",
+                "type": "rich",
+                "image": {
+                    "url": re.search(
+                        cls.PATTERN, results["entries"][0]["summary"]
+                    ).group(2),
+                },
+            }
+        )
 
 
 def publish(podcast: "Podcast"):
     embeded = Embed.from_dict(podcast)
+    webhook = Webhook.from_url(
+        "https://discord.com/api/webhooks/793195455194333186/Kvie1rOoBa28XyKb15epsnZmQ0EIQ16GnSonJ8Gfi1K4Wpn907mIcRpjRlhM6fCAKPrh",
+        adapter=RequestsWebhookAdapter(),
+    )
+    webhook.send(embed=embeded)
     print(embeded)
 
 
