bea50284722e6d0f951068f3c3b8ec964c6571d1
Ryan Long <ryan@ryanlong.tech>
2020-12-28 16:03:55 -0500

    Commit before DB integration.

--- src/blitzcrank/cogs/rss.py
+++ src/blitzcrank/cogs/rss.py
@@ -1,43 +1,106 @@
+import logging
 import feedparser
 import re
 from discord import Embed, Webhook, RequestsWebhookAdapter
+from feedparser.util import FeedParserDict
+
+logger = logging.getLogger(__name__)
 
 
 class Podcast(dict):
-    PATTERN = r"(src=\")(\S*)(\")"
+    """Represents podcast data """
 
     def __repr__(self):
         return f"{self.__dict__.items()}"
 
     @classmethod
-    def from_feed(cls, url):
-        results = feedparser.parse(url)
+    def fetch_feed(cls, url):
+        """Returns data from RSS feed
+
+        Args:
+            url (str): URL to fetch RSS data from
+
+        Returns:
+            FeedParserDict: data fetched
+        """
+        return feedparser.parse(url)
+
+    def publish(self, url):
+        """Publishes podcast to Discord as embed
+
+        Args:
+            podcast (Podcast): podcast to publish
+            webhook_url (str): Discord webhook URL
+        """
+
+        embeded = Embed.from_dict(self)
+        webhook = Webhook.from_url(
+            url,
+            adapter=RequestsWebhookAdapter(),
+        )
+        webhook.send(embed=embeded)
+
+
+class CodingBlocks(Podcast):
+    """Represents CodingBlocks podcast data """
+
+    IMAGE_URL_PATTERN = r"(src=\")(\S*)(\")"
+
+    @classmethod
+    def id(cls, results):
+        return results["entries"][0]["id"]
+
+    @classmethod
+    def description(cls, results):
+        return results["entries"][0]["subtitle"]
+
+    @classmethod
+    def url(cls, results):
+        return results["entries"][0]["link"]
+
+    @classmethod
+    def title(cls, results):
+        return f"{results['entries'][0]['itunes_title']} ({results['entries'][0]['itunes_episode']})"
+
+    @classmethod
+    def image(cls, results):
+        return {
+            "url": re.search(
+                cls.IMAGE_URL_PATTERN, results["entries"][0]["summary"]
+            ).group(2),
+        }
+
+    @classmethod
+    def from_feed(cls, url) -> Podcast:
+        """Returns Podcast object from feed
+
+        Args:
+            url ([type]): [description]
+
+        Returns:
+            Podcast: [description]
+        """
+        try:
+            results = super().fetch_feed(url)
+        except Exception as e:
+            logger.error(e)
+            return Podcast()
+
         return Podcast(
             {
-                "id": results["entries"][0]["id"],
-                "url": results["entries"][0]["link"],
-                "description": results["entries"][0]["subtitle"],
-                "title": f"{results['entries'][0]['itunes_title']} ({results['entries'][0]['itunes_episode']})",
+                "id": cls.id(results),
+                "url": cls.url(results),
+                "description": cls.description(results),
+                "title": cls.title(results),
                 "type": "rich",
-                "image": {
-                    "url": re.search(
-                        cls.PATTERN, results["entries"][0]["summary"]
-                    ).group(2),
-                },
+                "image": cls.image(results),
             }
         )
 
 
-def publish(podcast: "Podcast"):
-    embeded = Embed.from_dict(podcast)
-    webhook = Webhook.from_url(
-        "https://discord.com/api/webhooks/793195455194333186/Kvie1rOoBa28XyKb15epsnZmQ0EIQ16GnSonJ8Gfi1K4Wpn907mIcRpjRlhM6fCAKPrh",
-        adapter=RequestsWebhookAdapter(),
-    )
-    webhook.send(embed=embeded)
-    print(embeded)
-
-
 if __name__ == "__main__":
-    p = Podcast.from_feed("https://www.codingblocks.net/feed/podcast")
-    publish(p)
+    podcast = CodingBlocks.from_feed(
+        "https://www.codingblocks.net/feed/podcast"
+    ).publish(
+        "https://discord.com/api/webhooks/793195455194333186/Kvie1rOoBa28XyKb15epsnZmQ0EIQ16GnSonJ8Gfi1K4Wpn907mIcRpjRlhM6fCAKPrh"
+    )
